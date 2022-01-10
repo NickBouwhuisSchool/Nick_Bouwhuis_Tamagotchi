1a5e88db71e8f5bc2da39ff361d6ad1a80335b30
JustDisco <nick.bouwhuis@outlook.com>
2022-01-10 13:09:42 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nick_Bouwhuis_Tamagotchi
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
