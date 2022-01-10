7b4eee6bfbb2c5facfad67b30c1c38d3e958c6bd
JustDisco <nick.bouwhuis@outlook.com>
2022-01-10 13:35:28 +0100

    UserStory 1 and 2 v2
    ging iets mis ofzo

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Nick_Bouwhuis_Tamagotchi
 {
@@ -8,17 +9,29 @@ namespace Nick_Bouwhuis_Tamagotchi
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D tamTexture;
+        private SpriteFont creditFont;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferHeight = 512;
+            _graphics.PreferredBackBufferWidth = 512;
+
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            
+
+
+
+
+
 
             base.Initialize();
         }
@@ -26,6 +39,10 @@ namespace Nick_Bouwhuis_Tamagotchi
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            tamTexture = Content.Load<Texture2D>("tamagotchi");
+            creditFont = Content.Load<SpriteFont>("File");
 
             // TODO: use this.Content to load your game content here
         }
@@ -42,9 +59,19 @@ namespace Nick_Bouwhuis_Tamagotchi
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(tamTexture, Vector2.Zero, Color.White);
+            _spriteBatch.DrawString(creditFont, "Icon made by Webalys,\n https://www.flaticon.com/authors/webalys", Vector2.Zero, Color.White);
+
+
+
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
